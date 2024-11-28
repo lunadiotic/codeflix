0260f5e7ac69f8fc05571693c2c27a2d90e9e9be
ahmad hakim <113881962+aimgov@users.noreply.github.com>
2024-11-28 23:01:33 +0700

    seeder: make dummy category with seeder

---
+++ database/seeders/CategorySeeder.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+
+class CategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // List of movie categories
+        $categories = [
+            'Action',
+            'Adventure',
+            'Animation',
+            'Comedy',
+            'Crime',
+            'Documentary',
+            'Drama',
+            'Fantasy',
+            'Horror',
+            'Musical',
+            'Mystery',
+            'Romance',
+            'Science Fiction',
+            'Thriller',
+            'Western',
+        ];
+
+        // Insert each category into the database
+        foreach ($categories as $category) {
+            DB::table('categories')->insert([
+                'title' => $category,
+                'slug' => Str::of($category)->slug('-'),
+                'created_at' => now(),
+                'updated_at' => now(),
+            ]);
+        }
+    }
+}
