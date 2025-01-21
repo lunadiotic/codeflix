bd584682e01300804d09b1eed7358c9e246c6419
ahmad hakim <ahmad.hakim@wartek.belajar.id>
2025-01-21 15:34:25 +0700

    plan and membership: redirect after login

--- app/Providers/FortifyServiceProvider.php
+++ app/Providers/FortifyServiceProvider.php
@@ -8,9 +8,11 @@ use App\Actions\Fortify\UpdateUserPassword;
 use App\Actions\Fortify\UpdateUserProfileInformation;
 use Illuminate\Cache\RateLimiting\Limit;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Str;
+use Laravel\Fortify\Contracts\LoginResponse;
 use Laravel\Fortify\Contracts\RegisterResponse;
 use Laravel\Fortify\Fortify;
 
@@ -24,6 +26,18 @@ class FortifyServiceProvider extends ServiceProvider
         $this->app->instance(RegisterResponse::class, new class implements RegisterResponse {
             public function toResponse($request)
             {
+                return redirect()->route('subscribe.plans');
+            }
+        });
+
+        $this->app->instance(LoginResponse::class, new class implements LoginResponse {
+            public function toResponse($request)
+            {
+                // check if the user has a membership plan
+                if (Auth::user()->hasMembershipPlan()) {
+                    return redirect()->intended(config('fortify.home'));
+                }
+
                 return redirect()->route('subscribe.plans');
             }
         });
